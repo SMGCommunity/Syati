478a82afa1a828a53bc214c2b7b48a08436fd39e
SunakazeKun <nintendo.rpgforum@gmail.com>
2021-04-27 13:37:26 +0200

    update area stuff
    reorganized AreaObj, added AreaForm and AreaObjMgr

---
+++ include/Actor/AreaObj/AreaForm.h
@@ -0,0 +1,15 @@
+#pragma once
+
+enum AreaFormType {
+	AreaForm_Cube,
+	AreaForm_CenteredCube,
+	AreaForm_Sphere,
+	AreaForm_Cylinder,
+	AreaForm_Bowl
+};
+
+class AreaForm;
+
+namespace MR {
+	AreaForm* createAreaForm(AreaFormType);
+};
