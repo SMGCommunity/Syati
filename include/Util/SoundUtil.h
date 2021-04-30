513f354edf189460acf54daa4ac630e3fe8e2827
SunakazeKun <nintendo.rpgforum@gmail.com>
2021-04-30 02:42:15 +0200

    Update SoundUtil.h

--- include/Util/SoundUtil.h
+++ include/Util/SoundUtil.h
@@ -6,6 +6,8 @@ class LiveActor;
 
 namespace MR
 {
+	void startSystemSE(const char *, s32, s32);
+
 	void startSound(const LiveActor *, const char *, s32, s32);
 	void startSound(const LiveActor *, s32 JAISoundID, s32, s32);
 	void startLevelSound(const LiveActor *, const char *, s32, s32, s32);
