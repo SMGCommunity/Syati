bb6109dfd9cf4d48dfc97031c3d33836c641af8f
SuperHackio <superhaxio@gmail.com>
2021-05-01 19:30:20 -0700

    Introduce ActorCameraUtil

---
+++ include/Util/ActorCameraUtil.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "kamek.h"
+
+#include "Actor/LiveActor/LiveActor.h"
+#include "JMap/JMapInfoIter.h"
+
+namespace MR
+{
+	bool initActorCamera(const LiveActor *, const JMapInfoIter &)
+}
