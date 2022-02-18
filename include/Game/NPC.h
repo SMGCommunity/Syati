564998c92331a20455835aad2d892ce47bb9d583
SunakazeKun <nintendo.rpgforum@gmail.com>
2022-02-18 14:34:50 +0100

    Rosetta header

--- include/Game/NPC.h
+++ include/Game/NPC.h
@@ -2,6 +2,7 @@
 
 #include "Game/NPC/EventDirector.h"
 #include "Game/NPC/NPCActor.h"
+#include "Game/NPC/Rosetta.h"
 #include "Game/NPC/ScoreAttackCounter.h"
 #include "Game/NPC/Tico.h"
 #include "Game/NPC/TicoEat.h"
