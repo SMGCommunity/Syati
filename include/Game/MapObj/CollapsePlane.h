d23bab393adc341f9afd0399ecb79add981fbe82
Evanbowl <74565954+Evanbowl@users.noreply.github.com>
2023-11-25 14:38:41 -0500

    CollapsePlane

--- include/Game/MapObj/CollapsePlane.h
+++ include/Game/MapObj/CollapsePlane.h
@@ -12,14 +12,16 @@ public:
     virtual void calcAndSetBaseMtx();
     virtual void control();
     void calcJointPlane(TMtx34f* pMtx, const JointControllerInfo& info);
+    bool tryDPDStop();
+    virtual ~CollapsePlane();
 
     void exeReturn(); // NEW
 
-    s32 _D0;
-    s32 _D4;
-    s32 _D8;
-    s32 _DC;
-    s32 _E0;
+    AnimScaleController* mAnimScaleController; // 0xD0
+    WalkerStateBindStarPointer* mWalkerStateBindStarPointer; // 0xD4
+    JointControlDelegator<CollapsePlane>* mJointControllerDelegator; // 0xD8
+    s32 mCollapseTimer; // 0xDC
+    s32 mCollapseSpeed; // 0xE0
     s32 mRespawnTime; // 0xE4 NEW
 };
 
