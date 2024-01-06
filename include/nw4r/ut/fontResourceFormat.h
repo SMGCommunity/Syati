06797d4a7fc60b98c7a089ee428c6ced0e500c6f
Evanbowl <74565954+Evanbowl@users.noreply.github.com>
2024-01-05 23:28:31 -0500

    nw4r 2

--- include/nw4r/ut/fontResourceFormat.h
+++ include/nw4r/ut/fontResourceFormat.h
@@ -3,22 +3,23 @@
 #include "revolution\types.h"
 
 
-namespace nw4r::ut {
-
-struct CharWidths
-{
-    s8 left;                    // left space width of character
-    u8 glyphWidth;              // glyph width of character
-    s8 charWidth;               // character width  = left space width  + glyph width  + right space width
-};
-
-enum FontEncoding
-{
-    FONT_ENCODING_UTF8,
-    FONT_ENCODING_UTF16,
-    FONT_ENCODING_SJIS,
-    FONT_ENCODING_CP1252,
-    NUM_OF_FONT_ENCODING
-};
-
+namespace nw4r {
+        namespace ut {
+        
+        struct CharWidths
+        {
+            s8 left;                    // left space width of character
+            u8 glyphWidth;              // glyph width of character
+            s8 charWidth;               // character width  = left space width  + glyph width  + right space width
+        };
+    
+        enum FontEncoding
+        {
+            FONT_ENCODING_UTF8,
+            FONT_ENCODING_UTF16,
+            FONT_ENCODING_SJIS,
+            FONT_ENCODING_CP1252,
+            NUM_OF_FONT_ENCODING
+        };
+    };
 }
