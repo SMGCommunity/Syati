e4dddd0895e0336f07ba897e4149d9ca0f780aec
Super Hackio <superhaxio@gmail.com>
2024-03-13 16:45:56 -0700

    Create AirBubbleHolder.h

---
+++ include/Game/MapObj/AirBubbleHolder.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Game/LiveActor/LiveActorGroup.h"
+#include "JSystem/JGeometry/TVec.h"
+
+class AirBubbleHolder : public LiveActorGroup {
+public:
+    AirBubbleHolder(const char*);
+
+    virtual void init(const JMapInfoIter&);
+
+    void appearAirBubble(const TVec3f&, s32);
+};
+
+namespace MR {
+    AirBubbleHolder* createAirBubbleHolder();
+    void appearAirBubble(const TVec3f&, s32);
+};
