3ca37dce70a36399fed7793a41b8d604ff26ecf7
nico-apps <nicomarikucza@googlemail.com>
2024-08-06 20:10:35 +0200

    Document BombHei

---
+++ include/Game/Enemy/BombHei.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "Game/LiveActor.h"
+#include "Game/Util/JointController.h"
+
+class BombHei : public LiveActor {
+public:
+    BombHei(const char *);
+    ~BombHei();
+    virtual void init(const JMapInfoIter &);
+    virtual void initAfterPlacement();
+    virtual void appear();
+    virtual void kill();
+    virtual void calcAndSetBaseMtx();
+    virtual void control();
+    virtual void attackSensor(HitSensor *, HitSensor *);
+    virtual bool receiveMsgPlayerAttack(u32, HitSensor *, HitSensor *);
+    virtual bool receiveMsgEnemyAttack(u32, HitSensor *, HitSensor *);
+    virtual bool receiveMsgPush(u32, HitSensor *, HitSensor *);
+    virtual bool receiveOtherMsg(u32, HitSensor *, HitSensor *);
+    bool receiveMsgThrow(HitSensor *, HitSensor *);
+
+    void setGroupClipping(const JMapInfoIter &);
+    void startCountdown();
+    void endCountdown();
+    void exeLaunch();
+    void exeStarting();
+    void exeWait();
+    void exeWalk();
+    void exePursue();
+    void exePursueHit();
+    void exeSpinHit();
+    void exeTrample();
+    void exePhysics();
+    void exeStop();
+    void exeThrown();
+    void exeTaken();
+    void exeExplode();
+    void calcJoint(TPos3f *, JointControllerInfo &);
+    void startBoundSound();
+};
+
+namespace NrvBombHei {
+    NERVE(HostTypeNrvLaunch);
+    NERVE(HostTypeNrvStarting);
+    NERVE(HostTypeNrvWait);
+    NERVE(HostTypeNrvWalk);
+    NERVE(HostTypeNrvPursue);
+    NERVE(HostTypeNrvPursueFast);
+    NERVE(HostTypeNrvPursueHit);
+    NERVE(HostTypeNrvSpinHit);
+    NERVE(HostTypeNrvTrample);
+    NERVE(HostTypeNrvPhysics);
+    NERVE(HostTypeNrvStop);
+    NERVE(HostTypeNrvTaken);
+    NERVE(HostTypeNrvThrown);
+    NERVE(HostTypeNrvExplode);
+    NERVE(HostTypeNrvBindStarPointer);
+};
