32b477dc9947889a3da8de2a72c6109c4208356c
Nico Marikucza <nicomarikucza@googlemail.com>
2024-08-29 00:54:35 +0200

    Document MarioSwim

---
+++ include/Game/Player/MarioSwim.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Game/Player.h"
+
+class MarioSwim : public MarioState {
+    MarioSwim(MarioActor *);
+
+    virtual void init();
+    virtual bool start();
+    virtual bool update();
+    virtual bool notice();
+    virtual void draw3D() const;
+    virtual bool passRing(const HitSensor *);
+    virtual f32 getBlurOffset() const;
+    virtual f32 getStickY() const;
+    
+    void jet();
+    void resetJet();
+    void dropJet(bool);
+    void startJet(u32);
+    void pushedByWaterWall();
+    void hitPoly(u8, const TVec3f &, HitSensor *);
+    void hitWall(const TVec3f &, HitSensor *);
+    void incOxygen();
+    void decOxygen(u16);
+
+    void incLife();
+    void addVelocity(const TVec3f &);
+    void addVelocity(const TVec3f &, f32);
+    void updateTilt();
+    f32 getWaterEdgeDist() const;
+
+    u8 _0[0x208];
+};
