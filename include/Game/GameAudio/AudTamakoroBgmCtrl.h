86fd386eb30cd625d4038ecf69c2b09e57b3578f
Evanbowl <74565954+Evanbowl@users.noreply.github.com>
2024-11-08 18:57:26 -0500

    TamakoroBgmParamEntry

--- include/Game/GameAudio/AudTamakoroBgmCtrl.h
+++ include/Game/GameAudio/AudTamakoroBgmCtrl.h
@@ -4,6 +4,22 @@
 
 class JAISoundHandle;
 
+struct TamakoroBgmParamEntry {
+    const char* mName;
+    u32 _4;
+    u32 _8;
+    u32 _C;
+    f32 _10;
+    f32 _14;
+    f32 _18;
+    f32 _1C;
+    f32 _20;
+    u32 _24;
+    f32 _28;
+    u32 _2C;
+    u32 _30;
+};
+
 class AudTamakoroBgmCtrl {
 public:
 	AudTamakoroBgmCtrl();
@@ -21,7 +37,7 @@ public:
 	f32 _14;
 	s32 _18;
 	s32 _1C;
-	s32 _20;
+	TamakoroBgmParamEntry* mBgmParam;
 	s32 _24;
 	s32 _28;
 };
