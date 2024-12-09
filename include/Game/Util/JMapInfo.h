7a007bcdaa51f8d264aa07f1af42eb186464333c
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2024-12-09 11:10:48 -0600

    fully document JMapData

--- include/Game/Util/JMapInfo.h
+++ include/Game/Util/JMapInfo.h
@@ -11,10 +11,10 @@ struct JMapItem {
 };
 
 struct JMapData {
-    s32 _0;
+    s32 mNumEntries;
     s32 mNumData;      // _4
     s32 mDataOffset;   // _8
-    u32 _C;
+    u32 mEntrySize;
     JMapItem* mItems;  // _10
 };
 
