1363ca50dd969995ff539baeee509287d981ea33
shibbo <supershibbo12@gmail.com>
2024-12-11 14:42:37 -0500

    add exe actor macro for nerves

--- include/Game/LiveActor/Spine.h
+++ include/Game/LiveActor/Spine.h
@@ -26,6 +26,20 @@ public:\
     static name sInstance;\
 };\
 
+/* Declares a nerve and also defines the body of the nerve's execution function, which calls a specified member function */
+#define NERVE_DECL_EXE(name, parent_class, executor_name)\
+class name : public Nerve\
+{\
+public:\
+    name() NO_INLINE {\
+    };\
+    virtual void execute(Spine *pSpine) const {\
+        parent_class* actor = reinterpret_cast<parent_class*>(pSpine->mExecutor);\
+        actor->exe##executor_name();\
+    };\
+    static name sInstance;\
+};\
+
 class Spine {
 public:
     Spine(void *pExecutor, const Nerve *pNerve, s32 stateKeeperSize);
