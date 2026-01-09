896018a1100fdbe6091a4cae793d5fc5e21dab7e
Super Hackio <superhaxio@gmail.com>
2026-01-09 15:25:43 -0800

    New FileUtil function

--- include/Game/Util/FileUtil.h
+++ include/Game/Util/FileUtil.h
@@ -6,6 +6,8 @@
 namespace MR {
     bool isFileExist(const char *, bool);
     u32 getFileSize(const char*, bool);
+    // Unknown function here
+    bool makeObjectArchiveFileName(char* outFullPathBuffer, u32 bufferLength, const char* objectName); // returns true/false based on if the object archive exists or not
 
     // int should be JKRDvdRipper::EAllocDirection but whatever
     JKRArchive* loadToMainRAM(const char *, u8 *, JKRHeap *, int);
