a7f4b9745049ac3aa3103d0a561a16bf6f1ed5fd
Super Hackio <superhaxio@gmail.com>
2026-01-08 19:23:56 -0800

    mCameraInfo

--- include/Game/NPC/CollectTico.h
+++ include/Game/NPC/CollectTico.h
@@ -20,12 +20,12 @@ public:
 	void launch(const TPos3f&);
 	void startAppearPowerStar();
 
-	ActorCameraInfo* _90;     // 0x90
-	StrayTico** mStrayTicos;  // 0x94
-	s32 mTicoNum;             // 0x98
-	u8 _A8;                   // 0xA8
-	bool mHasCamera;          // 0xA9
-	bool mCompleteHere;       // 0xAA : ObjArg0 if set, the silver stars will do the completion animation at the position this object is placed
+	ActorCameraInfo* mCameraInfo; // 0x90
+	StrayTico** mStrayTicos;      // 0x94
+	s32 mTicoNum;                 // 0x98
+	u8 _A8;                       // 0xA8
+	bool mHasCamera;              // 0xA9
+	bool mCompleteHere;           // 0xAA : ObjArg0 if set, the silver stars will do the completion animation at the position this object is placed
 };
 
 namespace NrvCollectTico {
