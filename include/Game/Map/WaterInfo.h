38456adf2940f7ff3a8c3d83998dcbbc413cd394
Super Hackio <superhaxio@gmail.com>
2026-02-02 17:04:40 -0800

    Add Offset comments to WaterInfo

--- include/Game/Map/WaterInfo.h
+++ include/Game/Map/WaterInfo.h
@@ -16,17 +16,17 @@ public:
     bool isInWater() const;
     void clear();
 
-    f32 mCameraWaterDepth;
-    f32 _4;
-    TVec3f mSurfacePos;
-    TVec3f mSurfaceNormal;
-    f32 mWaveHeight;
-    TVec3f mStreamVec;
-    f32 mEdgeDistance;
-    TVec3f mEdgePos;
-    WaterArea* mWaterArea;
-    OceanBowl* mOceanBowl;
-    OceanRing* mOceanRing;
-    OceanSphere* mOceanSphere;
-    WaterStream* mWaterStream;
+    f32 mCameraWaterDepth;     // 0x00
+    f32 _4;                    // 0x04
+    TVec3f mSurfacePos;        // 0x08
+    TVec3f mSurfaceNormal;     // 0x14
+    f32 mWaveHeight;           // 0x20
+    TVec3f mStreamVec;         // 0x24
+    f32 mEdgeDistance;         // 0x30
+    TVec3f mEdgePos;           // 0x34
+    WaterArea* mWaterArea;     // 0x40
+    OceanBowl* mOceanBowl;     // 0x44
+    OceanRing* mOceanRing;     // 0x48
+    OceanSphere* mOceanSphere; // 0x4C
+    WaterStream* mWaterStream; // 0x50
 };
