eef4a969a63e761d95fb4be4fa082db8f9c0d243
Super Hackio <superhaxio@gmail.com>
2026-03-15 10:32:29 -0700

    Fix ActorJointCtrl & Add Members

--- include/Game/LiveActor/ActorJointCtrl.h
+++ include/Game/LiveActor/ActorJointCtrl.h
@@ -4,12 +4,16 @@
 #include "JSystem.h"
 #include "Game/LiveActor.h"
 
+class DynamicJointCtrlKeeper;
+class FaceJointCtrlKeeper;
+class IKJointCtrlHolder;
+
 class ActorJointCtrl {
 public:
 	ActorJointCtrl(LiveActor*);
 
-	void startDynamicCtrl(const char*, s32);
-	void endDynamicCtrl(const char*, s32);
+	void startDynamicCtrl(s32); // const char* removed in SMG2
+	void endDynamicCtrl(s32); // const char* removed in SMG2
 	//Rip resetDynamicCtrl
 	void startFaceCtrl(s32);
 	void endFaceCtrl(s32);
@@ -21,4 +25,11 @@ public:
 	void startUpdate();
 	void endUpdate();
 	void setCallBackFunction();
+
+	LiveActor* mActor;                         // 0x00
+	DynamicJointCtrlKeeper* mDynamicJointCtrl; // 0x04
+	FaceJointCtrlKeeper* mFaceJointCtrl;       // 0x08
+	IKJointCtrlHolder* mIKJointCtrl;           // 0x0C
+	bool _10;                                  // 0x10 : CallBack function related
+	bool mHasIKJointCtrl;                      // 0x11
 };
