44ed89afde2da5fe9aeeae7f47426fc8d87df127
Super Hackio <superhaxio@gmail.com>
2026-03-20 14:36:56 -0700

    File Loading related headers

--- include/Game/System/FileLoader.h
+++ include/Game/System/FileLoader.h
@@ -3,24 +3,25 @@
 #include "revolution.h"
 #include "JSystem.h"
 
+class FileLoaderThread;
 class ArchiveHolder;
-
-class FileHolder {
-public:
-    bool isExist(const char *) const;
-};
+class FileHolder;
+struct RequestFileInfo;
 
 class FileLoader {
 public:
     JKRArchive* receiveArchive(const char *);
-
     void getMountedArchiveAndHeap(const char *, JKRArchive **, JKRHeap **);
-
     void requestMountArchive(const char *, JKRHeap *, bool);
-
     JKRArchive* createAndAddArchive(void *, JKRHeap *, const char *);
+    bool isNeedToLoad(const char*) const;
+    const RequestFileInfo* getRequestFileInfoConst(const char*) const;
+    RequestFileInfo* addRequest(const char*);
 
-    u8 _0[0x24];
-    FileHolder* mHolder; // _24
-    ArchiveHolder* mArchiveHolder; // _28
+    FileLoaderThread* mLoaderThread;     // 0x00
+    OSMutex mMutex;                      // 0x04
+    RequestFileInfo* mRequestFileInfos;  // 0x1C
+    s32 mRequestedFileCount;             // 0x20
+    FileHolder* mHolder;                 // 0x24
+    ArchiveHolder* mArchiveHolder;       // 0x28
 };
---
+++ include/Game/System/FileLoaderThread.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Game/System/FileHolder.h"
+#include "Game/System/OSThreadWrapper.h"
+
+class FileHolderFileEntry;
+
+struct RequestFileInfo {
+    u32 _0;
+    s32 mRequestType;      // 0x4
+    char mFileName[0x80];  // 0x8
+    u32 _88;
+    FileHolderFileEntry* mFileEntry;  // 0x8C
+};
+
+class FileLoaderThread : public OSThreadWrapper {
+public:
+    FileLoaderThread(int, int, JKRHeap*);
+
+    virtual void* run();
+
+    void loadToMainRAM(RequestFileInfo*);
+    void mountArchiveAndStartCreateResource(RequestFileInfo*);
+};
